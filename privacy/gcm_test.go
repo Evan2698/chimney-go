f870849df2a4864c21b6c3e90aee912d876c68f7
Evan2698 <zwh2698@163.com>
2020-03-16 14:45:01 +0800

    o

--- privacy/gcm_test.go
+++ privacy/gcm_test.go
@@ -1,13 +1,24 @@
 package privacy
 
-import "testing"
+import (
+	"encoding/hex"
+	"strings"
+	"testing"
+)
 
 func TestGCM(t *testing.T) {
 
-	hello := NewMethodWithName("RAW")
-	fuck, err := hello.Compress([]byte("zhangweihua"), MakeCompressKey("kdkzhangweihua"))
-	t.Log(fuck, err)
-	one, err := hello.Uncompress(fuck, MakeCompressKey("kdkzhangweihua"))
+	hello := NewMethodWithName("AES-GCM")
+	hello.SetIV([]byte("123456789012"))
+	fuck, err := hello.Compress([]byte("Im a secret message!"), []byte("12345678901234567890123456789012"))
+
+	t.Log(strings.ToUpper(hex.EncodeToString(fuck)), err, len(fuck), strings.ToUpper(hex.EncodeToString(hello.ToBytes())))
+	one, err := hello.Uncompress(fuck, []byte("12345678901234567890123456789012"))
 	t.Log(string(one), err)
 
 }
+
+//12340C313233343536373839303132
+//12340C313233343536373839303132
+//C99FDD8F2232A48297774BA4BC701B2F7FD973D534DA9B9B08BA9C86F5F7167C21AAECF5
+//C99FDD8F2232A48297774BA4BC701B2F7FD973D534DA9B9B08BA9C86F5F7167C21AAECF5
