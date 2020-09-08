8d9698672afc32f78c59e8efcc03c27d9bbd0319
Evan2698 <zwh2698@163.com>
2020-09-08 21:38:16 +0800

    o

---
+++ monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while [ "1" = "1" ]
+do 
+    ps -efww |grep -v grep |grep vpn/chimney-go
+    ch=$?
+    if [ $? -ne 0 ]; then
+      nohup /home/evan/works/vpn/chimney-go -s &      
+    fi
+
+    sleep 30
+done
