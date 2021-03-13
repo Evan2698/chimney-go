f00e385748a0580c225cf03bda6add5037286b50
Evan2698 <zwh2698@163.com>
2021-03-13 09:33:20 +0800

    windows and linux support

--- socketcore/buildsocket.go
+++ socketcore/buildsocket.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"log"
 	"net"
-	"os"
 	"syscall"
 )
 
@@ -37,46 +36,7 @@ func TCPDail(host string, pFun mobile.ProtectSocket) (net.Conn, error) {
 	return outcon, nil
 }
 
-func connectSocketCoreBase(fd int,
-	ip net.IP, port int,
-	pFun mobile.ProtectSocket,
-	funAttr func(fd int) error) (net.Conn, error) {
 
-	sa, err := buildSocketAddress(ip, port)
-	if err != nil {
-		log.Println("construct network address failed!", err)
-		return nil, err
-	}
-
-	if pFun != nil {
-		ret := pFun.Protect(fd)
-		log.Println("protect socket: ", ret)
-	}
-
-	if funAttr != nil {
-		err = funAttr(fd)
-		if err != nil {
-			log.Println("set socket attrbuites failed!! ", err)
-			return nil, err
-		}
-	}
-
-	err = syscall.Connect(fd, sa)
-	if err != nil {
-		log.Println("connect remote end failed!!!!", err)
-		return nil, err
-	}
-
-	file := os.NewFile(uintptr(fd), "")
-	defer file.Close()
-
-	outcon, err := net.FileConn(file)
-	if err != nil {
-		log.Println("convert to FileConn failed:", err)
-		return nil, err
-	}
-	return outcon, nil
-}
 
 func buildSocketAddress(ip net.IP, port int) (sa syscall.Sockaddr, err error) {
 
